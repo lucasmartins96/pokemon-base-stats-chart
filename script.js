6c4726da0279989bcd65576f35cbfd95e4fd6b3e
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-19 00:49:55 -0300

    Configuraçao do layout da pagina

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const configureData = () => {
   return {
     labels: ['HP', 'Attack', 'Special Attack', 'Speed', 'Special Defense', 'Defense'],
     datasets: [{
-      label: 'example',
+      label: '{pokemon}',
       data: [70, 80, 60, 50, 80, 100],
       fill: true,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
-      pointBackgroundColor: 'rgb(255, 99, 132)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgb(255, 99, 132)'
+      backgroundColor: 'rgb(3, 4, 94, 0.2)',
+      borderColor: '#03045eff',
+      pointBackgroundColor: '#fff',
+      pointBorderColor: '#03045eff',
+      pointHoverBackgroundColor: '#03045eff',
+      // pointHoverBorderColor: '#fff'
     }]
   }
 };
@@ -30,7 +30,18 @@ const configureChart = (data) => {
         line: {
           borderWidth: 3
         }
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: '{Pokemon} Base Stats'
+        }
       }
+      /* layout: {
+        padding: {
+            bottom: 150
+        }
+      } */
     },
   }
 };
