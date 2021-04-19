3f7bfd9c62569d694dd90270164c7b53e1c5e9e4
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-18 22:04:08 -0300

    Configuraçao da exibiçao do chart com informaçoes genericas

--- script.js
+++ script.js
@@ -3,39 +3,45 @@
     .then((response) => response.json())
 } */
 
-var ctx = document.getElementById('myChart');
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+const configureData = () => {
+  return {
+    labels: ['HP', 'Attack', 'Special Attack', 'Speed', 'Special Defense', 'Defense'],
+    datasets: [{
+      label: 'example',
+      data: [70, 80, 60, 50, 80, 100],
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(255, 99, 132)'
+    }]
+  }
+};
+
+const configureChart = (data) => {
+  return {
+    type: 'radar',
+    data: data,
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+      elements: {
+        line: {
+          borderWidth: 3
         }
-    }
-});
+      }
+    },
+  }
+};
+
+const renderChart = () => {
+  const ctx = document.getElementById('myChart');
+  const data = configureData();
+  const chartConfigurations = configureChart(data);
+  const myChart = new Chart(ctx, chartConfigurations);
+};
+
+window.onload = () => {
+  renderChart();
+};
