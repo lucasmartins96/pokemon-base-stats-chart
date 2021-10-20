39a6c53c5b87037152e513fa9bbdeb4128970539
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-20 13:48:39 -0300

    test(App): Verifica se há 1 título, 1 botão e 2 parágrafos
    O título deve conter o texto "Pokemon Base Status Chart"
    O botão deve conter o texto "Start"
    O conteúdos dos parágrafos estão no arquivo paragraphsContent

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import '@testing-library/jest-dom';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from './App';
+import renderWithRouter from './utils/renderWithRouter';
+import paragraphsContent from './utils/paragraphsContent';
 
-test("renders learn react link", () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+test('Deve renderizar 1 título, 1 botão e 2 parágrafos', () => {
+  renderWithRouter(<App />);
+  const p1 = screen.getByText(paragraphsContent.homeP1);
+  const p2 = screen.getByText(paragraphsContent.homeP2);
+
+  expect(
+    screen.getByRole('heading', { name: /Pokémon Base Status Chart/ })
+  ).toBeInTheDocument();
+  expect(p1).toBeInTheDocument();
+  expect(p2).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Start' })).toBeInTheDocument();
 });
---
+++ src/utils/paragraphsContent.js
@@ -0,0 +1,5 @@
+export default {
+  homeP1:
+    'Informe o nome ou ID da National Dex do seu Pokémon favorito e visualize o status base.',
+  homeP2: `Você pode comparar o status base com outros Pokémon's se quiser.`,
+};
