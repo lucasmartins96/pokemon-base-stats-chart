c9e28903c2853d3a36d2fc2f410fcd7af93bfd5f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-20 13:50:58 -0300

    feat(Home ): adicionando os elementos do componente Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
+import Button from 'react-bootstrap/Button';
 import React from 'react';
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <p>This is the home page</p>
+      <h1>Pokémon Base Status Chart</h1>
+      <p>
+        Informe o nome ou ID da National Dex do seu Pokémon favorito e visualize o status
+        base.
+      </p>
+      <p>Você pode comparar o status base com outros Pokémon's se quiser.</p>
+      <Button variant="primary">Start</Button>
     </div>
   );
 }
